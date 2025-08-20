e589b4ae82a7e805b811069edda294aa9c0653b6
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-08-20 12:57:37 +0800

    Update powershell.sh

--- powershell.sh
+++ powershell.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install wget -y
 source /etc/os-release
-wget -q https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 packages.microsoft.com
