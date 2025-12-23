036b8c45d5b2bb6a036651ae304655a61bcb8efa
Willie169 <cg10250207@gmail.com>
2025-12-23 20:47:33 +0800

    l

--- PostgreSQL.sh
+++ PostgreSQL.sh
@@ -1,3 +1,6 @@
 sudo apt install postgresql-common -y
 sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
 sudo apt install postgresql-17 -y
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
