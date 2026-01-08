013dfe1b1031f18728d4cd30421b2c76e43ce111
Willie Shen <cg10250207@gmail.com>
2026-01-08 20:41:50 +0800

    y

--- MySQL.sh
+++ MySQL.sh
@@ -1,5 +1,5 @@
 wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.36-1_all.deb
-sudo apt install ./mysql-apt-config_0.8.36-1_all.deb -y
+sudo apt install ./mysql-apt-config_0.8.36-1_all.deb -y # ok
 rm mysql-apt-config_0.8.36-1_all.deb
 sudo apt update
 sudo apt install mysql-community-server -y
