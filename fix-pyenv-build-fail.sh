e76bfe664d30b4581abf4db570930fc1ea85d908
Willie Shen <cg10250207@gmail.com>
2026-01-15 19:41:08 +0800

    a

--- fix-pyenv-build-fail.sh
+++ fix-pyenv-build-fail.sh
@@ -2,4 +2,4 @@ sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
 libzstd-dev
-python3 -m ensurepip --default-pip
+export CXXFLAGS='-std=c++17'
