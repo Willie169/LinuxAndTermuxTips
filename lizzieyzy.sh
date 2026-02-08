299ba35fac017b5cd5a5b2e6b70adb1aa32f814c
Willie Shen <cg10250207@gmail.com>
2026-02-08 13:33:40 +0800

    b

--- lizzieyzy.sh
+++ lizzieyzy.sh
@@ -1,3 +1,4 @@
+cd ~
 git clone https://github.com/yzyray/lizzieyzy.git
 cd lizzieyzy
 git checkout 2.5.3
@@ -6,4 +7,4 @@ cd ..
 ## Run with
 # java -jar lizzieyzy/target/lizzie-yzy2.5.3-shaded.jar
 ## You may set an alias for it. Example:
-# alias lizzieyzy='java -jar lizzieyzy/target/lizzie-yzy2.5.3-shaded.jar'
+# alias lizzieyzy='java -jar $HOME/lizzieyzy/target/lizzie-yzy2.5.3-shaded.jar'
