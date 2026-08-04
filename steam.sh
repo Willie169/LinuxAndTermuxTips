3b6047c85599888b790db32fdd6756f45469554f
Willie Shen <cg10250207@gmail.com>
2026-08-04 16:02:53 +0800

    2026-08-04T08:02:53+00:00

---
+++ steam.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget --tries=100 --retry-connrefused --waitretry=5 https://cdn.fastly.steamstatic.com/client/installer/steam.deb
+sudo apt install ./steam.deb -y
+rm steam.deb*
