654e8611e86ca9b08322a120d1ebc93143842813
Willie Shen <cg10250207@gmail.com>
2026-08-17 12:19:44 +0800

    2026-08-17T04:19:44+00:00

---
+++ proot-install-nethunter.sh
@@ -0,0 +1,6 @@
+## mb outdated?
+pkg update
+pkg install git ncurses-utils proot-distro -y
+git clone https://github.com/sagar040/proot-distro-nethunter.git
+cd proot-distro-nethunter
+./install-nethunter.sh --install
